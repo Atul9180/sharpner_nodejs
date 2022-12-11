7d358f8f4a79df72b63ede54f6be2f03e2ad2dc3
Atul <patelatulpatel@gmail.com>
2022-12-12 00:59:53 +0530

    Parsing body form data using bodyparser

--- Starting_Express_Framework/index.js
+++ Starting_Express_Framework/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
 const port = 3000;
 
-//this middleware will run for all
-app.use((req,res,next) => {
-    console.log('in middleware 1!');
-    next();                             //alows the req to continue to next middleware in line
+app.use(bodyParser.urlencoded({extended:false}));  //parse theform data from body
+
+//added '/add-product' middleware prior to '/' middleware. else everytime even for '/add-prod...or other '/' middleware works
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action="/product">Product Name:<input type="text" name="title" /><br />Product Size:<input type="text" name="size" /><br /><button type="submit">Add Product</button></form>') 
 });
 
-app.use((req,res,next)=>{
-    console.log('in middleware 2!');
-    res.send('<h1>Hello from express</h1>');
+app.use('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next) => {
+    res.send('<h1>Hi this is HomePage.</h1>')
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
